a18cf7c0f3ed20237e43c897f6d9a09e7cdda7b0
StylianosGakis <stylianos.gakis0006@stud.hkr.se>
2019-05-03 20:24:03 +0200

    Add connection through php
    Testing the connection with the database through php

---
+++ connection.php
@@ -0,0 +1,15 @@
+<?
+$servername = "localhost";
+$username = "da216agroup15";
+$dbname = "da216agroup15";
+$password = "<da216a>";
+//Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+//Check connection
+if(!$conn){
+	die("Connectioned failed!" . mysqli_connect_error());
+}
+else{
+	echo "Connect success!";
+}
+?>
